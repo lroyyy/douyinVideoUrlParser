3af35e82a5b3493848c02733644bfdd82bb59325
getfei <getfei@nebula.com>
2020-03-21 15:53:47 +0800

    first

---
+++ src/main/java/cn/getfei/douyinVideoUrlParser/DouyinVideoUrlParserApplication.java
@@ -0,0 +1,13 @@
+package cn.getfei.douyinVideoUrlParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouyinVideoUrlParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouyinVideoUrlParserApplication.class, args);
+	}
+
+}
